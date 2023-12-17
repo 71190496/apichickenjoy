734912143ef0de64b85d17bb3672b0ebca6f171b
John Julius Pattikaihatu <john.julius@ti.ukdw.ac.id>
2023-12-17 08:49:55 +0700

    respons auth, menu & api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\QueryException;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
@@ -22,72 +23,109 @@ class AuthController extends Controller
             ]);
         }
 
-        // $request->validate([
-        //     'username' => 'required|unique:users',
-        //     'password' => 'required|min:6',
-        //     'nama_karyawan' => 'required',
-        // ]);
+        try {
+            $request->validate([
+                'username' => 'required|unique:users',
+                'password' => 'required|min:6',
+                'nama_karyawan' => 'required',
+            ]);
 
-        $user = User::create([
-            'username' => $request->username,
-            'nama_karyawan' => $request->input('nama_karyawan'),
-            'password' => Hash::make($request->password),
-            'role' => 'karyawan',
-        ]);
-        // dd($user);
+            $user = User::create([
+                'username' => $request->username,
+                'nama_karyawan' => $request->input('nama_karyawan'),
+                'password' => Hash::make($request->password),
+                'role' => 'karyawan',
+            ]);
 
-
-
-        $user->id_user;
-        $user->save();
-
-        return response()->json([
-            'statusCode' => 200,
-            'message' => 'Registrasi Berhasil',
-            'user' => $user->only(['id_user', 'nama_karyawan', 'username', 'role']),
-
-        ]);
+            return response()->json([
+                'statusCode' => 200,
+                'message' => 'Registrasi Berhasil',
+                'user' => $user->only(['id_user', 'nama_karyawan', 'username', 'role']),
+            ]);
+        } catch (ValidationException $e) {
+            // Penanganan kesalahan validasi
+            return response()->json([
+                'statusCode' => 400,
+                'message' => 'Registrasi Gagal',
+                'errors' => $e->errors(),
+            ]);
+        } catch (QueryException $e) {
+            // Penanganan kesalahan basis data (misalnya, unik constraint)
+            return response()->json([
+                'statusCode' => 400,
+                'message' => 'Registrasi Gagal',
+                'errors' => ['username' => ['Username sudah digunakan']],
+            ]);
+        } catch (\Exception $e) {
+            // Penanganan kesalahan umum
+            return response()->json([
+                'statusCode' => 500,
+                'message' => 'Terjadi kesalahan server.',
+            ]);
+        }
     }
 
 
     public function login(Request $request)
     {
-        $request->validate([
-            'username' => 'required',
-            'password' => 'required',
-        ]);
-
-        $user = User::where('username', $request->username)->first();
-
-        if (!$user || hash('sha256', $request->password) !== $user->password) {
-            throw ValidationException::withMessages([
-                'username' => ['The provided credentials are incorrect.'],
+        try {
+            $request->validate([
+                'username' => 'required',
+                'password' => 'required',
             ]);
+
+            $user = User::where('username', $request->username)->first();
+
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                throw ValidationException::withMessages([
+                    'message' => ['The provided credentials are incorrect.'],
+                ]);
+            }
+
+            // Tambahkan kondisi untuk memeriksa peran admin
+            if ($user->role !== 'admin' && $user->role != 'karyawan') {
+                throw ValidationException::withMessages([
+                    'message' => ['Unauthorized access.'],
+                ]);
+            }
+
+            // Buat token berdasarkan peran pengguna
+            $tokenName = $user->role === 'admin' ? 'Login Admin' : 'Login Karyawan';
+            $plainTextToken = $user->createToken($tokenName)->plainTextToken;
+
+            $response = [
+                'statusCode' => 200,
+                'message' => 'Login Berhasil',
+                'user_id' => $user->id_user,
+                'token_name' => $tokenName,
+                'plain_text_token' => $plainTextToken,
+            ];
+
+            // Convert the array to JSON and return the JSON response
+            return response()->json($response);
+        } catch (ValidationException $e) {
+            // Penanganan kesalahan validasi (kredensial salah, akses tidak diizinkan)
+            return response()->json([
+                'statusCode' => 401,
+                'message' => 'Password Salah',
+                'errors' => $e->errors(),
+            ], 401);
+        } catch (\Exception $e) {
+            // Penanganan kesalahan umum
+            return response()->json([
+                'statusCode' => 500,
+                'message' => 'Internal Server Error',
+            ], 500);
         }
-
-        // Tambahkan kondisi untuk memeriksa peran admin
-        if ($user->role !== 'admin' && $user->role != 'karyawan') {
-            throw ValidationException::withMessages([
-                'username' => ['Unauthorized access.'],
-            ]);
-        }
-
-        // Buat token berdasarkan peran pengguna
-        $tokenName = $user->role === 'admin' ? 'Login Admin' : 'Login Karyawan';
-        $plainTextToken = $user->createToken($tokenName)->plainTextToken;
-
-        $response = [
-            'user_id' => $user->id_user,
-            'token_name' => $tokenName,
-            'plain_text_token' => $plainTextToken,
-        ];
-
-        // Convert the array to JSON and return the JSON response
-        return response()->json($response);
     }
 
     public function logout(Request $request)
     {
         $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'statusCode' => 200,
+            'message' => 'Logout berhasil',
+        ]);
     }
 }
--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -2,50 +2,66 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Menu;
-use App\Http\Resources\MenuResource;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Http\Resources\MenuResource;
+use Illuminate\Validation\ValidationException;
 
 class MenuController extends Controller
 {
     public function index(Request $request)
     {
-        $query = Menu::query();
-
-        if ($request->has('kategori')) {
-            $query->where('kategori', $request->kategori);
-        }
-
-        $menu = $query->get();
+        $menu = Menu::all();
 
         if ($menu->isEmpty()) {
-            return response()->json(['message' => 'Kategori tidak ditemukan'], 404);
+            return response()->json(['statusCode' => 400, 'message' => 'Menu ditemukan',], 404);
         }
 
-        return response()->json(['statusCode' => 200 ,'data' =>   MenuResource::collection($menu)] ,200);
+        return response()->json(['statusCode' => 200, 'message' => 'Menu ditemukan', 'data' => MenuResource::collection($menu)], 200);
+    }
+
+
+    public function indexByCategory($kategori)
+    {
+        $query = Menu::query();
+        $query->where('kategori', $kategori);
+
+        $menus = $query->get();
+
+        if ($menus->isEmpty()) {
+            return response()->json(['statusCode' => 404, 'message' => 'Kategori tidak ditemukan'], 404);
+        }
+
+        return response()->json(['statusCode' => 200, 'message' => 'Kategori ditemukan', 'data' => MenuResource::collection($menus)], 200);
     }
 
 
     public function store(Request $request)
     {
-        $request->validate([
-            'nama_menu' => 'required',
-            'harga' => 'required|numeric',
-            'kategori' => 'required',
-        ]);
+        try {
+            $request->validate([
+                'nama_menu' => 'required',
+                'harga' => 'required|numeric',
+                'kategori' => 'required',
+            ]);
 
-        $menu = Menu::create([
-            'nama_menu' => $request->input('nama_menu'),
-            'harga' => $request->input('harga'),
-            'kategori' => $request->input('kategori'),
-        ]);
+            $menu = Menu::create([
+                'nama_menu' => $request->input('nama_menu'),
+                'harga' => $request->input('harga'),
+                'kategori' => $request->input('kategori'),
+            ]);
 
-        return response()->json(['message' => 'Menu berhasil ditambahkan', 'data' => new MenuResource($menu)], 201);
+            return response()->json(['statusCode' => 201, 'message' => 'Menu berhasil ditambahkan', 'data' => new MenuResource($menu)], 201);
+        } catch (ValidationException $e) {
+            // Tanggapi jika validasi gagal
+            return response()->json(['statusCode' => 422, 'message' => 'Validasi gagal', 'errors' => $e->errors()], 422);
+        }
     }
 
     public function update(Request $request, $id)
     {
+        try {
         $request->validate([
             'nama_menu' => 'required',
             'harga' => 'required|numeric',
@@ -64,7 +80,11 @@ class MenuController extends Controller
             'kategori' => $request->input('kategori'),
         ]);
 
-        return response()->json(['message' => 'Menu berhasil diperbarui', 'data' => new MenuResource($menu)], 200);
+        return response()->json(['statusCode' => 201, 'message' => 'Menu berhasil diedit', 'data' => new MenuResource($menu)], 201);
+        } catch (ValidationException $e) {
+            // Tanggapi jika validasi gagal
+            return response()->json(['statusCode' => 422, 'message' => 'Validasi gagal', 'errors' => $e->errors()], 422);
+        }
     }
 
     public function destroy($id_menu)
@@ -72,11 +92,11 @@ class MenuController extends Controller
         $menu = Menu::find($id_menu);
 
         if (!$menu) {
-            return response()->json(['error' => 'Menu tidak ditemukan'], 404);
+            return response()->json(['statusCode' => 404, 'error' => 'Menu tidak ditemukan'], 404);
         }
 
         $menu->delete();
 
-        return response()->json(['message' => 'Menu berhasil dihapus'], 200);
+        return response()->json(['statusCode' => 200, 'message' => 'Menu berhasil dihapus'], 200);
     }
 }
