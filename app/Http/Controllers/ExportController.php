4d839c3e67cc4b461bb6c21494582475f1fc3411
patrick paletei <patrick.pranaduta@ti.ukdw.ac.id>
2024-01-02 03:31:33 +0700

    add update export

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 
-use App\Models\User;
+
 use App\Models\Pesanan;
 use App\Exports\ExcelExport;
 use Illuminate\Http\Request;
@@ -13,18 +13,7 @@ class ExportController extends Controller
 {
     public function export()
     {
-        // Mengambil semua pengguna yang memiliki role 'admin'
-        $admins = User::where('role', 'admin')->get();
-
-        // Memeriksa apakah pengguna saat ini termasuk dalam daftar admin
-        $currentUser = auth()->user();
-        if (!$admins->contains($currentUser)) {
-            return response()->json([
-                'statuscode' => 403,
-                'message' => 'Unauthorized. Only admins can export data.'], 403);
-        }
-
-        $invoices = Pesanan::with('menu', 'transaksi.user')->get()->groupBy('menu.kategori');
+        $invoices = Pesanan::with('menu', 'transaksi.user')->get()->groupBy('menu.kategori'); // Retrieve data from your model
 
         return Excel::download(new ExcelExport($invoices), 'invoices.xlsx');
     }
